a8891906eb66e4ff0107f19ac306893a6444e9e2
Aryan296 <aryan0296.be21@chitkara.edu.in>
2024-07-11 13:02:21 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+
+function App() {
+  return (
+    <div className="container1">
+      <Header/>
+    </div>
+  );
+}
+export default App;
